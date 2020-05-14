17d9763836e23f534e9199a7e23055d7f7ece3da
ElizabethTorre <etr604@gmail.com>
2020-05-14 02:17:57 -0500

    deploy install

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Signup } from './views/welcome';
-//import { Waiter } from './views/waiter';
 import { Login } from './views/login';
 import './css/App.css';
 
@@ -14,9 +13,6 @@ export const App = () => {
         <Route path="/welcome">
           <Signup />
         </Route>
-        {/*<Route path="/waiter">
-          <Waiter />
-        </Route>*/}
         <Route path="/">
           <Login />
         </Route>
@@ -24,30 +20,3 @@ export const App = () => {
     </Router>
   );
 };
-
-//import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-
-//function App() {
-//  return (
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//      </header>
-//    </div>
-//  );
-//}
-
-//export default App;
