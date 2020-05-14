2256bde9bf2b99be60ac36f6f473d5cf83135b74
ElizabethTorre <etr604@gmail.com>
2020-05-14 09:23:23 -0500

    Updates

---
+++ precache-manifest.5df6fa4be5aebd3f86a6487215d7f807.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7802b91e3aa058b42e8e2e5851c2f9b2",
+    "url": "/ReactLogin/index.html"
+  },
+  {
+    "revision": "9971379d0762d12a486b",
+    "url": "/ReactLogin/static/css/main.0b667d86.chunk.css"
+  },
+  {
+    "revision": "3c9430e28028b990c9d5",
+    "url": "/ReactLogin/static/js/2.5f7549ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactLogin/static/js/2.5f7549ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9971379d0762d12a486b",
+    "url": "/ReactLogin/static/js/main.d9c2fb31.chunk.js"
+  },
+  {
+    "revision": "1a169b440403810ca18a",
+    "url": "/ReactLogin/static/js/runtime-main.256d73e4.js"
+  },
+  {
+    "revision": "4d28fcf2585a1cd35f79790d0bad6f9a",
+    "url": "/ReactLogin/static/media/administrador.4d28fcf2.png"
+  }
+]);
